12f126659b9dc44061a42505fe077992154c9d77
Felix Thiel <github@thiel.green>
2017-10-31 16:34:33 +0100

    Failing test for #9

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -9,4 +9,16 @@ describe "authors index page page", type: :feature do
         visit authors_path
         expect(page).to have_link("Add author")
     end
+
+    it "should have a Link to the authors pages" do
+        # create an author first so that there is something to show
+        visit new_author_path
+        fill_in "author_first_name", :with => 'Alan'
+        fill_in "author_last_name", :with => 'Turing'
+        fill_in "author_homepage", :with => 'http://wikipedia.org/Alan_Turing'
+        find('input[type="submit"]').click
+        
+        visit authors_path
+        expect(page).to have_link("Show")
+    end
 end
