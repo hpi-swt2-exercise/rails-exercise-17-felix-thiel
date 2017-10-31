a707d0ee06eee73326a1e5e9c58de2512acdd69f
Felix Thiel <github@thiel.green>
2017-10-31 16:00:44 +0100

    Failing test for #8

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -4,4 +4,9 @@ describe "authors index page page", type: :feature do
     it "should render" do
         visit authors_path
     end
+
+    it "should have a Link to the new author page" do
+        visit authors_path
+        expect(page).to have_link("New Author")
+    end
 end
