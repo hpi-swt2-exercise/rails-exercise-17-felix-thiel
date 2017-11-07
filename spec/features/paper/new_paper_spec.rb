2ae7d68a8e0cb13e54988ce2bdcc3c6e3a324758
Felix Thiel <github@thiel.green>
2017-11-07 08:37:31 +0100

    added failing test for #14

--- spec/features/paper/new_paper_spec.rb
+++ spec/features/paper/new_paper_spec.rb
@@ -5,4 +5,14 @@ describe "New paper page", type: :feature do
     it "should render without error" do
         visit new_paper_path
     end
+
+    it "should not validate without title" do
+        visit new_paper_path
+        fill_in "paper_title", :with => ''
+        fill_in "paper_venue", :with => 'Mind 49: 433-460'
+        fill_in "paper_year", :with => 1950
+        find('input[type="submit"]').click
+
+        expect(page).to have_text("Title can't be blank")
+    end
 end
