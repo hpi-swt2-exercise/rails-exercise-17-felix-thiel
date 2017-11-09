13d27d64f2e5d611784818fb20dac7b47b88a089
Felix Thiel <github@thiel.green>
2017-11-09 09:24:33 +0100

    fixed failing test for #22

--- spec/features/paper/paper_page_spec.rb
+++ spec/features/paper/paper_page_spec.rb
@@ -4,10 +4,9 @@ describe "Paper detail page", type: :feature do
     it "should have the authors name" do
         author = FactoryGirl.create :author
         paper = FactoryGirl.create :paper
-        #paper.author = author
 
-        visit papers_path(paper)
+        visit paper_path(paper)
 
-        expect(page).to have_text("mystring mystring")
+        expect(page).to have_text(paper.authors.first.name)
     end
 end
